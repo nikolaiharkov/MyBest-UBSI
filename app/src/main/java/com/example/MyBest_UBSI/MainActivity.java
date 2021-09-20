dca7018c3bf03020b272935b8bec12d2fb88cf35
Nikolai Harkov <74813988+nikolaiharkov@users.noreply.github.com>
2021-09-20 11:43:17 +0700

    Download file Update
    my webview can download file now, but it still need another browser, and sometimes got error when you need download a file who need some authorized

--- app/src/main/java/com/example/MyBest_UBSI/MainActivity.java
+++ app/src/main/java/com/example/MyBest_UBSI/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.MyBest_UBSI;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.DownloadListener;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -18,14 +21,28 @@ public class MainActivity extends Activity {
         mWebView = findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new MyWebViewClient());
 
+        mWebView.setDownloadListener(new DownloadListener() {
+            public void onDownloadStart(String url, String userAgent,
+                                        String contentDisposition, String mimetype,
+                                        long contentLength) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        mWebView.setWebViewClient(new MyWebViewClient());
         // REMOTE RESOURCE
          mWebView.loadUrl("http://elearning.bsi.ac.id/login");
 
         // LOCAL RESOURCE
         // mWebView.loadUrl("file:///android_asset/index.html");
+
     }
+
+
+
 
 
     @Override
